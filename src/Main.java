1907abbe1a18812c49b1fa1379d0d0fd21b6bcce
07elbekdev <masrapovelbek24@gmail.com>
2022-11-18 23:29:43 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Class number = new Class(1,"Welcome to our class !!!",2023);
+        System.out.println(number);
+    }
+}
