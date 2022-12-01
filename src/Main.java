5b8cb92d11cd2851a2c267d3cd8ab18a16007759
07elbekdev <masrapovelbek24@gmail.com>
2022-12-01 23:52:43 +0600

    All thing became differently

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Class number = new Class(1,"Welcome to our class !!!",2023);
-        System.out.println(number);
+        Scanner in = new Scanner(System.in);//Зарплата задание.
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        int[] array = {a, b, c};
+        int max = Arrays.stream(array).max().getAsInt();
+        int min = Arrays.stream(array).min().getAsInt();
+        int count = 0;
+        for (int i = min; i < max; i++) {
+            count++;
+        }
+        System.out.println(count);
     }
 }
