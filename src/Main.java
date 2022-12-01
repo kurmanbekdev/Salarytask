bc45679c243d255881d331f57f5df5f74a9eede8
07elbekdev <masrapovelbek24@gmail.com>
2022-12-01 23:56:51 +0600

    All thing became up to date

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);//Зарплата задание.
+        Scanner in = new Scanner(System.in);
         int a, b, c;
         a = in.nextInt();
         b = in.nextInt();
         c = in.nextInt();
+        operations(a, b, c);
+    }
+
+    public static void operations(int a, int b, int c) {
         int[] array = {a, b, c};
         int max = Arrays.stream(array).max().getAsInt();
         int min = Arrays.stream(array).min().getAsInt();
